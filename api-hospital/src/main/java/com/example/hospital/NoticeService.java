ae2e3db176d6aecd0d23134af67609e2b1ea6211
dong hwi <ehdgnl3465@naver.com>
2025-02-22 13:13:50 +0900

    [Feat] 병원 공지사항 기능

---
+++ api-hospital/src/main/java/com/example/hospital/NoticeService.java
@@ -0,0 +1,42 @@
+package com.example.hospital;
+
+import com.example.hospital.notice.Notice;
+import com.example.hospital.notice.NoticeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+
+    public void register(NoticeDto.Register dto) {
+        noticeRepository.save(dto.toEntity());
+    }
+
+    public List<Notice> list() {
+        List<Notice> NoticeList = noticeRepository.findAll();
+
+        return NoticeList;
+    }
+
+    public NoticeDto.Response read(Long idx) {
+        Optional<Notice> result = noticeRepository.findById(idx);
+
+        if (result.isPresent()) {
+            Notice templete = result.get();
+
+            return NoticeDto.Response.from(templete);
+        }
+        return null;
+    }
+
+    public NoticeDto.Response searchByName(String hospitalId) {
+        Notice notice = noticeRepository.findByHospitalId(hospitalId).orElseThrow();
+
+        return NoticeDto.Response.from(notice);
+    }
+}
