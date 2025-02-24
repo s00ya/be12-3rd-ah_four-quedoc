0e7e6db0e64fcd043de2a228dcff1999887fbad3
j0a0j <jiweon.j.kim@gmail.com>
2025-02-24 17:56:21 +0900

    payment 기본 설정. 다른 모듈과 연결은 되는 상태.

--- api-payment/src/main/java/com/example/payment/PaymentApplication.java
+++ api-payment/src/main/java/com/example/payment/PaymentApplication.java
@@ -2,8 +2,11 @@ package com.example.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.example.payment.model", "com.example.admin.user.model"})
 public class PaymentApplication {
 
     public static void main(String[] args) {
