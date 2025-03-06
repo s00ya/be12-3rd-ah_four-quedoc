996532d2edc7ec2ff3b7856105c68867e93ec721
scy <tjcksdud0221@naver.com>
2025-03-06 16:06:02 +0900

    병원의 모든 예약 조회 기능 추가

--- api-reservation/src/main/java/com/example/apireservation/reservation/model/Reservation.java
+++ api-reservation/src/main/java/com/example/apireservation/reservation/model/Reservation.java
@@ -7,8 +7,6 @@ import jakarta.persistence.*;
 import com.example.hospital.hospital.model.Hospital;
 import lombok.*;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,26 +21,24 @@ public class Reservation {
     @Schema(description = "Reservation 테이블의 primary key, auto_increment", example = "1")
     private long idx;
 
-    @ManyToOne
-    @JoinColumn(name = "user_idx", nullable = false)
-    @Schema(description = "예약한 사람의 정보")
-    private User user; // user foreign key
+    @Column(nullable = false)
+    @Schema(description = "예약된 병원의 primary key", example = "1")
+    private Long hospitalIdx;
 
-    @ManyToOne
-    @JoinColumn(name = "hospital_idx", nullable = false)
-    @Schema(description = "예약된 병원의 정보")
-    private Hospital hospital; // hospital foreign key
+    @Column(nullable = false)
+    @Schema(description = "예약한 사람의 primary key", example = "1")
+    private Long userIdx;
 
     @Column(nullable = false)
     @Schema(description = "병원 방문자의 이름", example = "홍길동")
     private String name;
 
-    @Column(nullable = false, columnDefinition = "TIMESTAMP")
-    @Schema(description = "예약 시간", example = "2025-03-01")
-    private LocalDateTime time;
+    @Column(nullable = false)
+    private String phoneNumber;
 
-    @PrePersist
-    protected void onCreate() {
-        time = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES); // 초 제거
-    }
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean isApproved;
+
 }
