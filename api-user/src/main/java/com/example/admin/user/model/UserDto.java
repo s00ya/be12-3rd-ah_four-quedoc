dbf45c0f0a9df9a7171d6e2069411fe53689ba23
scy <tjcksdud0221@naver.com>
2025-03-06 10:58:50 +0900

    user dto 수정

--- api-user/src/main/java/com/example/admin/user/model/UserDto.java
+++ api-user/src/main/java/com/example/admin/user/model/UserDto.java
@@ -44,8 +44,8 @@ public class UserDto {
 
     @Getter
     @Setter
-    @Schema(description = "유저 회원가입을 할때 사용하는 dto")
-    public static class SignupDto {
+    @Schema(description = "일반 사용자 회원가입을 할때 사용하는 dto")
+    public static class UserSignupDto {
         @NotBlank(message = "이메일을 입력하세요.")
         @Email(message = "유효한 이메일 형식이 아닙니다.")
         private String email;
@@ -77,9 +77,56 @@ public class UserDto {
 
     @Getter
     @Setter
+    @Schema(description = "사업자 회원가입을 할때 사용하는 dto")
+    public static class BusinessSignupDto {
+        @NotBlank(message = "이메일을 입력하세요.")
+        @Email(message = "유효한 이메일 형식이 아닙니다.")
+        private String email;
+
+        @NotBlank(message = "이름을 입력하세요.")
+        @Size(min = 2, max = 20, message = "이름은 2~20자 사이여야 합니다.")
+        private String name;
+
+        @NotBlank(message = "비밀번호를 입력하세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+                message = "비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다."
+        )
+        private String password;
+
+        @NotBlank(message = "닉네임을 입력하세요.")
+        @Size(min = 3, max = 15, message = "닉네임은 3~15자 사이여야 합니다.")
+        private String nickname;
+
+        @NotBlank(message = "전화번호를 입력하세요.")
+        @Pattern(
+                regexp = "^01[0-9]\\d{7,8}$",
+                message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)"
+        )
+        private String phoneNumber;
+
+        private String businessNumber;
+
+        private String customerTypeCode;
+    }
+
+    @Getter
+    @Setter
     @Builder
     @Schema(description = "유저 정보 응답할때 사용하는 dto")
-    public static class ResponseDto {
+    public static class UserResponseDto {
+        private Long idx;
+        private String email;
+        private String name;
+        private String nickname;
+        private String type;
+    }
+    @Getter
+    @Setter
+    @Builder
+    @Schema(description = "유저 정보 응답할때 사용하는 dto")
+    public static class BusinessResponseDto {
+        private Long idx;
         private String email;
         private String name;
         private String nickname;
